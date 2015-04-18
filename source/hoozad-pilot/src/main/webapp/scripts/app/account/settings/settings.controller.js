33d90cb2a622675b230374b3ec4e399ba0f30cde
Alex Fernandez <alexfdz@gmail.com>
2015-04-18 11:17:18 +0100

    #14: Added delivery details form to the existing settings form

--- source/hoozad-pilot/src/main/webapp/scripts/app/account/settings/settings.controller.js
+++ source/hoozad-pilot/src/main/webapp/scripts/app/account/settings/settings.controller.js
@@ -4,11 +4,14 @@ angular.module('hoozadApp')
     .controller('SettingsController', function ($scope, Principal, Auth) {
         $scope.success = null;
         $scope.error = null;
+        $scope.placeOptions = { types: ['(cities)']};
+
         Principal.identity().then(function(account) {
             $scope.settingsAccount = account;
         });
 
         $scope.save = function () {
+            $scope.settingsAccount.deliveryDetails.city = $scope.settingsAccount.deliveryDetails.city.formatted_address;
             Auth.updateAccount($scope.settingsAccount).then(function() {
                 $scope.error = null;
                 $scope.success = 'OK';
