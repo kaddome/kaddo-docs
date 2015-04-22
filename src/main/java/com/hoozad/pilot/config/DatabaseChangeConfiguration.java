ba4e6841e70a2dc95230ff1058a653811bb9d96b
Alex Fernandez <alexfdz@gmail.com>
2015-04-22 10:17:07 +0100

    DB configuration for Heroku

---
+++ src/main/java/com/hoozad/pilot/config/DatabaseChangeConfiguration.java
@@ -0,0 +1,40 @@
+package com.hoozad.pilot.config;
+
+import com.mongodb.Mongo;
+import org.mongeez.Mongeez;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.inject.Inject;
+
+import static com.hoozad.pilot.config.Constants.SPRING_PROFILE_INTEGRATION;
+
+@Configuration
+@Profile("!fast")
+public class DatabaseChangeConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+    @Inject
+    private Mongo mongo;
+
+    @Inject
+    private MongoProperties mongoProperties;
+
+    @Bean
+    @Profile("!" + SPRING_PROFILE_INTEGRATION)
+    public Mongeez mongeez() {
+        log.debug("Configuring Mongeez");
+        Mongeez mongeez = new Mongeez();
+        mongeez.setFile(new ClassPathResource("/config/mongeez/master.xml"));
+        mongeez.setMongo(mongo);
+        mongeez.setDbName(mongoProperties.getDatabase());
+        mongeez.process();
+        return mongeez;
+    }
+}
